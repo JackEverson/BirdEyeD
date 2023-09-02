db47d5fe3647e412736904f1d4b758c2f4cd5bd3
Jack Everson <jack.everson@outlook.com>
2023-09-02 18:18:01 +1000

    messing with settings

--- training_model/training.py
+++ training_model/training.py
@@ -44,29 +44,32 @@ imshow(torchvision.utils.make_grid(images))
 print(' '.join('%s' %classes[labels[j]] for j in range(batch_size)))
 
 # starting to train CNN
-class Net(nn.Module):
-    # models a simple Convolutional Neural Network
-    def __init__(self):
-        # initalize the Network
-        super(Net, self).__init__()
-        # 3 input image channel, 6 output channels, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        # max pooling over a (2, 2) Window to give a 5X5 image
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)# 5x5 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+def setup_net():
+    class Net(nn.Module):
+        # models a simple Convolutional Neural Network
+        def __init__(self):
+            # initalize the Network
+            super(Net, self).__init__()
+            # 3 input image channel, 6 output channels, 5x5 square convolution kernel
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            # max pooling over a (2, 2) Window to give a 5X5 image
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)# 5x5 from image dimension
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        # forward propagation algorithm
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        def forward(self, x):
+            # forward propagation algorithm
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 16 * 5 * 5)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+setup_net()
 
 net = Net()
 print(net)
